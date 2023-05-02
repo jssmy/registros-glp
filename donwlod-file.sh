27276bf568c7e6bca0e8dd1af23329e81cbe99fc
jmanihuariy <joset.manihuari@telefonica.com>
2023-05-01 22:36:28 -0500

    jzsip installed

--- donwlod-file.sh
+++ donwlod-file.sh
@@ -11,3 +11,4 @@ file="$folder/file.zip"
 curl -o $file  https://www.osinergmin.gob.pe/seccion/centro_documental/hidrocarburos/SCOP/SCOP-DOCS/2023/Registro-precios/CL-Registro-precios-DMA-V-CCA-CCE-2023.zip
 unzip -q $file -d $folder
 rm -f $file
+node app.js
