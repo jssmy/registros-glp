60eb42c6fab0beadce97b3ce271a8c87111f4416
jmanihuariy <joset.manihuari@telefonica.com>
2023-05-01 23:33:04 -0500

    upadted index

---
+++ index.js
@@ -0,0 +1,15 @@
+const XLSX = require('xlsx');
+const path = require("path");
+const { Readable } = require('stream');
+
+const PATH_FILE = path.join(__dirname, 'extracted','CL-Registro-precios-DMA-V-CCA-CCE-2023_0.xlsx');
+function  read() {
+    const workbook = XLSX.readFile(PATH_FILE);
+    const workbookSheets = workbook.SheetNames;
+    const sheet = workbookSheets[0];
+    XLSX.stream.set_readable(Readable);
+    const dataExcel = XLSX.utils.sheet_to_csv(workbook.Sheets[sheet]);
+    console.log(dataExcel);
+}
+
+read();
