3f2a33c3c1224d0c8afa25faea869d1dd8d15864
jmanihuariy <joset.manihuari@telefonica.com>
2023-05-01 21:26:19 -0500

    actualizando scrip´t

--- donwlod-file.sh
+++ donwlod-file.sh
@@ -1,6 +1,10 @@
-if [! -d "extracted" ]
+folder = "extracted";
+if [! -d "$HOME/$folder" ]
 then
+  echo "Folder no existe $HOME/$folder"
   mkdir "extracted"
+else
+  echo "Folder si existe $HOME/$folder"
 fi
 
 curl -o extracted/file.zip https://www.osinergmin.gob.pe/seccion/centro_documental/hidrocarburos/SCOP/SCOP-DOCS/2023/Registro-precios/CL-Registro-precios-DMA-V-CCA-CCE-2023.zip
