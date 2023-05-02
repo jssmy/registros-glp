6ecbd33c71c33acee4c19ac275da065df131f59a
jmanihuariy <joset.manihuari@telefonica.com>
2023-05-01 21:31:54 -0500

    actualizando script 3

--- donwlod-file.sh
+++ donwlod-file.sh
@@ -1,10 +1,11 @@
-folder="extracted"
-if [! -d "$HOME/$folder" ]
+folder="$HOME/cron/registros-glp"
+
+if [! -d  "$folder"]
 then
-  echo "Folder no existe $HOME/$folder"
-  mkdir "extracted"
+  echo "Folder no existe $folder"
+  mkdir $folder
 else
-  echo "Folder si existe $HOME/$folder"
+  echo "Folder si existe $folder"
 fi
 
 curl -o extracted/file.zip https://www.osinergmin.gob.pe/seccion/centro_documental/hidrocarburos/SCOP/SCOP-DOCS/2023/Registro-precios/CL-Registro-precios-DMA-V-CCA-CCE-2023.zip
