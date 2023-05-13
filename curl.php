5a4ba7929de61902ce36e70ccd956f3d1fe3ca8e
jmanihuariy <joset.manihuari@telefonica.com>
2023-05-13 17:57:59 -0500

    update download file

--- curl.php
+++ curl.php
@@ -1,20 +1,34 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-$url  = 'https://www.osinergmin.gob.pe/seccion/centro_documental/hidrocarburos/SCOP/SCOP-DOCS/2023/Registro-precios/CL-Registro-precios-DMA-V-CCA-CCE-2023.zip';
-$fileName = "file.zip";
-$path = "/home/peruenergyadviso/app-process-file/extracted/$fileName";
-echo "extracted in $path";
+class DownLoadZipFile {
+    public function get($url, $downloadTo) {
+        $zipFile = "$downloadTo/".basename($url); // Rename .zip file        
+        $zipResource = fopen("$zipFile", "w");
 
-$fp = fopen($path, 'w');
+        // Get The Zip File From Server
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER,true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); 
+        curl_setopt($ch, CURLOPT_FILE, $zipResource);
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_FILE, $fp);
+        $page = curl_exec($ch);
 
-$data = curl_exec($ch);
+        if(!$page) {
+            echo "Error :- ".curl_error($ch);
+        }
 
-curl_close($ch);
-fclose($fp);
+        curl_close($ch);
+        return "$zipFile";
+    }
+};
 
-echo "-extraccion terminado-";
+
+$donloadTo = __DIR__;
+$zipFile = (new DownLoadZipFile())
+->get("https://www.osinergmin.gob.pe/seccion/centro_documental/hidrocarburos/SCOP/SCOP-DOCS/2023/Registro-precios/CL-Registro-precios-DMA-V-CCA-CCE-2023.zip", $donloadTo);
