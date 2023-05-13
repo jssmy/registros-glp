2ee60baff1c960de054b70d3783bb257bf97b54f
jmanihuariy <joset.manihuari@telefonica.com>
2023-05-13 17:33:45 -0500

    enabled 3errors

--- curl.php
+++ curl.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $url  = 'https://www.osinergmin.gob.pe/seccion/centro_documental/hidrocarburos/SCOP/SCOP-DOCS/2023/Registro-precios/CL-Registro-precios-DMA-V-CCA-CCE-2023.zip';
 $fileName = "file.zip";
 $path = "/home/peruenergyadviso/app-process-file/extracted/$fileName";
