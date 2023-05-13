6dc5eecc37c0581c95d75fcdd07e14af13a4f7da
jmanihuariy <joset.manihuari@telefonica.com>
2023-05-13 17:22:12 -0500

    updating php folder

--- download-file.sh
+++ download-file.sh
@@ -1,4 +1,5 @@
-folder="$HOME/app-process-file/extracted"
+mainfolder = "$HOME/app-process-file"
+folder="$mainfolder/extracted"
 
 excelName="CL-Registro-precios-DMA-V-CCA-CCE-2023_0.xlsx"
 file="$folder/file.zip"
@@ -26,7 +27,7 @@ echo "*****     DESCARGANDO ARCHIVO   *****";
 echo ""
 #curl -o $file  https://www.osinergmin.gob.pe/seccion/centro_documental/hidrocarburos/SCOP/SCOP-DOCS/2023/Registro-precios/CL-Registro-precios-DMA-V-CCA-CCE-2023.zip
 #echo "Downloaded file $file"
-/usr/bin/php curl.php
+/usr/bin/php $mainfolder/curl.php
 unzip  -o $file -d $folder
 rm -f "$folder/file.txt"
 rm -f $file
