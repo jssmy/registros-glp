c3c7fe9be0cafe1404d974324d1593688808a3b8
jmanihuariy <joset.manihuari@telefonica.com>
2023-05-27 16:09:58 -0500

    quitando caracteres extraños

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const { escape } = require('mysql');
 class Utils {
      static convertirNumeroExcelAFecha = (numeroExcel) => {
         const fechaBaseExcel = new Date("1900-01-01");
@@ -19,15 +20,18 @@ class Utils {
 
     static replaceWorseChars = (value) => {
         if(!value) return value;
-        
-        return value.replace('<t>', '')
+        utf
+        return decodeURIComponent(escape(value))
+        .replace('<t>', '')
           .replace('</t>', '')
           .replace('<v>', '')
           .replace('</v>', '')
           .replace('<t xml:space="preserve">', '')
           .replace(/[']+/g, '')
+          .replace(/["]+/g, '')
           .replace(/[\x96]+/g, '')
           .replace(/[\xC2]+/g, '')
+          .replace(/[\x92]+/g, '')
           .replace(/[\x93]+/g, '')
           .replace(/[\x91]+/g, '')
           .replace(/[\x94]+/g, '')
@@ -45,7 +49,10 @@ class Utils {
           .replace('</v>', '')
           .replace('<t xml:space="preserve">', '')
           .replace(/[']+/g, '');
+          Text
       }
+
+      static encode_utf8(s){ return unescape(encodeURIComponent(s));}
       
 }
 
