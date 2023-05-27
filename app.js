078a1f8072f3b18e8d1510dbff62dce9d0e61555
jmanihuariy <joset.manihuari@telefonica.com>
2023-05-27 15:19:53 -0500

    fixed lo

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ const mainFunction = () => {
           index ++;
           fecha_registro = Utils.convertirFechaDeExcelANormal(parseInt(Utils.replaceWorseChars(fecha_registro))).format('Y-MM-DD');
           
-          const beforeDay = moment().subtract(1, 'day').format('Y-MM-DD');
+          const beforeDay = moment().subtract(1, 'days').format('Y-MM-DD');
           console.log(fecha_registro, beforeDay);
           // valid last day
           if(fecha_registro == beforeDay) {
